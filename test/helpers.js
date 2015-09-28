c2247d286c1a70673c049e38f1fbf19c61a10af7
mijikim <miji.kim926@gmail.com>
2015-09-27 21:34:11 -0700

    fix test for profiler

--- test/helpers.js
+++ test/helpers.js
@@ -2,7 +2,6 @@
 
 var localChai = require('chai'),
   localSinon = require('sinon'),
-  Measured = require('measured');
 
 localChai.use(require('sinon-chai'));
 
@@ -12,40 +11,3 @@ global.expect = localChai.expect;
 global.should = localChai.should();
 
 global.sinon = localSinon;
-
-global.createStat = new Statham();
-
-function Statham() {
-  this._collection = {
-    // Number of requests that completed successfully
-    completedRequests: new Measured.Counter(),
-    latency: new Measured.Histogram(),
-    // Response codes, such as 200 for HTTP.
-    codes: {
-    },
-    cpu: new Measured.Histogram(),
-    memory: new Measured.Histogram()
-  };
-  return this;
-}
-
-Statham.prototype.addCode = function(code) {
-  if (!this._collection.codes[code]) {
-    this._collection.codes[code] = new Measured.Counter();
-  }
-  this._collection.codes[code].inc();
-
-  return this;
-};
-
-Statham.prototype.completedRequest = function() {
-  this._collection.completedRequests.inc();
-
-  return this;
-};
-
-Statham.prototype.addLatency = function(delta) {
-  this._collection.latency.update(delta);
-
-  return this;
-};
--- test/lib/profiler_test.js
+++ test/lib/profiler_test.js
@@ -1,145 +1,67 @@
 'use strict';
 
-var modulePath = '../../lib/profiler',
-  Measured = require('measured'),
+var EventEmitter = require('events').EventEmitter;
+  modulePath = '../../lib/profiler',
   proxyquire = require('proxyquire');  
 
 describe('profiler structure', function() {
-  var profiler = require(modulePath),
-    pid = 34562,
-    intermediate = {},
-    aggregate = {},
-    pendingRequests = {},
-    delta = 345,
-    code = 200;
+  var Profiler = require(modulePath),
+    config = {
+      plugins: {
+        profiler: {pid: 34562}
+      }
+    };
+    ee = new EventEmitter();
 
   it('returns updateUsage function', function() {
     expect(profiler.name).to.equal('updateUsage');
   });
 
-  it('validates inputs', function() {
+  it('has report function', function() {
+    var profiler = new Profiler(config, ee);
+    expect('report' in profiler).to.equal(true);
+  });
+
+  it('validates pid', function() {
+    config.plugins.profiler.pid = 'hi';
+
     expect(function() {
-      profiler();
+      new Profiler(config, ee);
     }).to.throw('pid (number) is required');
-
-    expect(function() {
-      profiler(pid);
-    }).to.throw('intermediate (object) is required');
-
-    expect(function() {
-      profiler(pid, intermediate);
-    }).to.throw('aggregate (object) is required');
-
-    expect(function() {
-      profiler(pid, intermediate, aggregate);
-    }).to.throw('pendingRequests (object) is required');
-
-    expect(function() {
-      profiler(pid, intermediate, aggregate, pendingRequests);
-    }).to.throw('delta (number) is required');
-
-    expect(function() {
-      profiler(pid, intermediate, aggregate, pendingRequests, delta);
-    }).to.throw('code (number) is required');
   });
-});
-
-
-describe('profiler does not run successfully', function() {
-  var intermediate,
-    aggregate,
-    pendingRequests,
-    pid = 34562,
-    delta = 345,
-    code = 200;
-
-  var usageOptions = {keepHistory: true};
-  var error = 'Error Message';
-
-  beforeEach(function() {
-    pendingRequests = new Measured.Counter();
-    intermediate = createStat;
-    aggregate = createStat;
-    
-    var usageStub = {
-      lookup: function(pid, usageOptions, cb) {
-        cb(error, {'cpu': 12.3, 'memory': 456778});
-      }
-    };
-
-    var Profiler = proxyquire(modulePath, {
-      'usage': usageStub
-    })
-    
-    new Profiler(pid, intermediate, aggregate, pendingRequests, delta, code);
-  });
-
-  it('updates aggregate', function() {
-    expect(aggregate._collection.cpu._min).to.not.equal(12.3);
-    expect(aggregate._collection.memory._min).to.not.equal(456778);
-    expect(aggregate._collection.latency._min).to.not.equal(345);
-    expect(aggregate._collection.codes).to.not.have.property('200');
-  });
-
 });
 
 describe('profiler runs successfully', function() {
-
-  var intermediate,
-    aggregate,
-    pendingRequests,
-    pid = 34562,
-    delta = 345,
-    code = 200;
-
-  var usageOptions = {keepHistory: true};
-
-  beforeEach(function() {
-    pendingRequests = new Measured.Counter();
-    intermediate = createStat;
-    aggregate = createStat;
-    
-    var usageStub = {
+  var usageOptions = {keepHistory: true},
+    usageStub = {
       lookup: function(pid, usageOptions, cb) {
         cb(null, {'cpu': 12.3, 'memory': 456778});
       }
-    };
-
-    var Profiler = proxyquire(modulePath, {
+    },
+    Profiler = proxyquire(modulePath, {
       'usage': usageStub
-    })
+    }),
+    config = {
+      plugins: {
+        profiler: {pid: 34562}
+      }
+    };
+    ee = new EventEmitter();
     
-    new Profiler(pid, intermediate, aggregate, pendingRequests, delta, code);
-  });
+  it('listens to EventEmitter', function() {
+    var profiler = new Profiler(config, ee);
 
-  it('adds functions to intermediate object', function() {
-    expect(intermediate.addCPU).to.be.an('function');
-    expect(intermediate.addMemory).to.be.an('function');
-  });
+    ee.emit('phaseStarted');
+    var report = profiler.report();
+    expect(report).to.equal(null);
 
-  it('adds functions to aggregate object', function() {
-    expect(aggregate.addCPU).to.be.an('function');
-    expect(aggregate.addMemory).to.be.an('function');
-  });
+    ee.emit('stats', {timestamp: '2015-09-22T22:47:53.505Z'});
+    var report = profiler.report();
+    expect('timestamp' in report[0]).to.equal(true);
+    expect(report[0].timestamp).to.equal('2015-09-22T22:47:53.505Z');
 
-  it('calls pendingRequests', function() {
-    expect(pendingRequests).to.have.property('dec');
+    ee.emit('phaseCompleted');
+    var report = profiler.report();
+    expect(report[1].timestamp).to.equal('aggregate');
   });
-
-  it('updates intermediate', function() {
-    expect(intermediate._collection.cpu._min).to.equal(12.3);
-    expect(intermediate._collection.memory._min).to.equal(456778);
-    expect(intermediate._collection.latency._min).to.equal(345);
-    expect(intermediate._collection.codes).to.have.property('200');
-  });
-
-  it('updates aggregate', function() {
-    expect(aggregate._collection.cpu._min).to.equal(12.3);
-    expect(aggregate._collection.memory._min).to.equal(456778);
-    expect(aggregate._collection.latency._min).to.equal(345);
-    expect(aggregate._collection.codes).to.have.property('200');
-  });
-
 });
-
-
